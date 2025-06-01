519d18a6c0f6743eb74d8df58294b4106660f5fb
emirkugic <emir.kugic@hotmail.com>
2025-06-01 09:36:49 +0200

    Update CORS configuration and .htaccess

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,8 @@
 <?php
-
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*', 'api/*', 'sanctum/csrf-cookie'],  // Allow all paths 
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000')],
+    'allowed_origins' => ['http://localhost:3000', 'https://streaming.araneum.ba'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
